518ae0bfdc92ef47aca65643888b29d365521361
meetsundrani <41486000+meetsundrani@users.noreply.github.com>
2020-12-06 22:55:55 +0530

    name undefined error

--- projbackend/controllers/category.js
+++ projbackend/controllers/category.js
@@ -2,24 +2,58 @@ const Category = require("../models/category");
 
 exports.getCategoryById = (req, res, next, id) => {
   Category.findById(id).exec((err, cate) => {
-    if (err || !cate) {
-      return res.status(400).json({
-        error: "Category Not Found",
-      });
+    if (err) {
+      return res.status(400).json({ error: "category not found" });
     }
+
     req.category = cate;
     next();
   });
 };
-
 exports.createCategory = (req, res) => {
   const category = new Category(req.body);
-  Category.save((err, category) => {
-    if (err || !category) {
+  category.save((err, category) => {
+    if (err) {
       return res.status(400).json({
-        error: "Category Not Found in DB",
+        error: "NOT able to save category in DB",
       });
     }
     res.json({ category });
+  });
+};
+
+exports.getCategory = (req, res) => {
+  return res.json(req.category);
+};
+
+exports.getAllCategories = (req, res) => {
+  Category.find().exec((err, category) => {
+    if (err) {
+      return res.status(400).json({ error: "No Category Found" });
+    }
+    res.json({ category });
+  });
+};
+
+exports.updateCategory = (req, res) => {
+  const category = req.category;
+  category.name = req.body.name;
+  category.save((err, updatedCategory) => {
+    if (err) {
+      return res.status(400).json({ error: "Category not updated in DB" });
+    }
+    res.json(updatedCategory);
+  });
+};
+
+exports.removeCategory = (req, res) => {
+  const category = req.category;
+  category.remove((err, category) => {
+    if (err) {
+      return res
+        .status(400)
+        .json({ error: "fail to delete ${category} in DB" });
+    }
+    res.json({ message: "${category}successfully deleted" });
   });
 };
--- projbackend/routes/category.js
+++ projbackend/routes/category.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { getCategoryById, createCategory } = require("../controllers/category");
+const {
+  getCategoryById,
+  createCategory,
+  getCategory,
+  getAllCategories,
+  updateCategory,
+  removeCategory,
+} = require("../controllers/category");
 const { getUserById } = require("../controllers/user");
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 
@@ -10,12 +17,37 @@ router.param("userId", getUserById);
 router.param("categoryId", getCategoryById);
 
 //actual routes goes here
+
+//create route
 router.post(
   "/category/create/:userId",
+  isSignedIn,
   isAuthenticated,
   isAdmin,
-  isSignedIn,
   createCategory
 );
-router.get("/category/get/:userId");
+//read routes
+router.get("/category/:categoryId", getCategory);
+router.get("/categoryies", getAllCategories);
+
+//update routes
+router.put(
+  "/category/:creategoryId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateCategory
+);
+
+//delete route
+router.delete(
+  "/category/:categoryId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  removeCategory
+);
+
+//mymiddleware
+
 module.exports = router;
