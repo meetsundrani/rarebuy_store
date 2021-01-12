7151ec0b63ec24b975b8e8aeace75ba146b6455e
meetsundrani <41486000+meetsundrani@users.noreply.github.com>
2021-01-12 23:19:58 +0530

    laptop-work

--- projfrontend/src/admin/AddCategory.js
+++ projfrontend/src/admin/AddCategory.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React,{useState} from "react";
+import { Link } from "react-router-dom";
 import Base from "../core/Base";
+import {isAutheticated} from "../auth/helper/index"
 const AddCategory = () => {
+const [name,setName]=useState("initialState");
+const [error,setError]=useState(false);
+const [success, setSuccess] = useState(false);
+
+const {user,token}=isAutheticated();
+
+const myCategoryForm=()=>(
+  <form>
+  <div className="form-group">
+    <p className="lead">Enter the category</p>
+    <input type="text" className="form-control my-3" autoFocus required placeholder="For Ex. Summer"/>
+    <button className="btn btn-outline-info">Create Category</button>
+  </div>
+  
+  </form>)
+
+
   return (
     <Base
       title="Create a Category"
