999790a35efce582be7484d46341cf672646da15
meetsundrani <41486000+meetsundrani@users.noreply.github.com>
2021-01-12 14:50:57 +0530

    till module16_1

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -6,7 +6,7 @@ import { signin, authenticate, isAutheticated } from "../auth/helper";
 
 const Signin = () => {
   const [values, setValues] = useState({
-    email: "m@gmail.com",
+    email: "meetsundrani@gmail.com",
     password: "12345",
     error: "",
     loading: false,
