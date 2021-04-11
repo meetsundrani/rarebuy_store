35d46a66959755b3e3053131412809b89f4b268f
meetsundrani <41486000+meetsundrani@users.noreply.github.com>
2021-04-12 00:02:15 +0530

    cart backend is up

--- projfrontend/src/core/Card.js
+++ projfrontend/src/core/Card.js
@@ -1,29 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { Redirect } from "react-router";
-import { addItemToCart } from "./helper/cartHelper";
 import ImageHelper from "./helper/ImageHelper";
-const Card = ({ product, removeFromCart = false }) => {
-  const cartTitle = product ? product.name : "A photo from pixel";
-  const cartDescription = product ? product.description : "Default Description";
-  const cartPrice = product ? product.price : "default price";
+import { Redirect } from "react-router-dom";
+import { addItemToCart } from "./helper/cartHelper";
 
+const Card = ({ product, addtoCart = true, removeFromCart = false }) => {
   const [redirect, setRedirect] = useState(false);
-  const [count, setcount] = useState(product.count);
+  const [count, setCount] = useState(product.count);
+
+  const cartTitle = product ? product.name : "A photo from pexels";
+  const cartDescrption = product ? product.description : "Default description";
+  const cartPrice = product ? product.price : "DEFAULT";
 
   const addToCart = () => {
     addItemToCart(product, () => setRedirect(true));
   };
+
   const getARedirect = (redirect) => {
     if (redirect) {
       return <Redirect to="/cart" />;
     }
   };
 
-  const showAddToCart = () => {
+  const showAddToCart = (addtoCart) => {
     return (
-      addToCart && (
+      addtoCart && (
         <button
-          onClick={addToCart()}
+          onClick={addToCart}
           className="btn btn-block btn-outline-success mt-2 mb-2"
         >
           Add to Cart
@@ -31,7 +33,8 @@ const Card = ({ product, removeFromCart = false }) => {
       )
     );
   };
-  const showRemoveFromCart = () => {
+
+  const showRemoveFromCart = (removeFromCart) => {
     return (
       removeFromCart && (
         <button
@@ -43,7 +46,6 @@ const Card = ({ product, removeFromCart = false }) => {
       )
     );
   };
-
   return (
     <div className="card text-white bg-dark border border-info ">
       <div className="card-header lead">{cartTitle}</div>
@@ -51,12 +53,12 @@ const Card = ({ product, removeFromCart = false }) => {
         {getARedirect(redirect)}
         <ImageHelper product={product} />
         <p className="lead bg-success font-weight-normal text-wrap">
-          {cartDescription}
+          {cartDescrption}
         </p>
-        <p className="btn btn-success rounded  btn-sm px-4">${cartPrice}</p>
+        <p className="btn btn-success rounded  btn-sm px-4">$ {cartPrice}</p>
         <div className="row">
-          <div className="col-12">{showAddToCart()}</div>
-          <div className="col-12">{showRemoveFromCart()}</div>
+          <div className="col-12">{showAddToCart(addtoCart)}</div>
+          <div className="col-12">{showRemoveFromCart(removeFromCart)}</div>
         </div>
       </div>
     </div>
